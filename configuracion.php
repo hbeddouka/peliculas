6efc945b4cfc0bdc22628913595c6d184908a91b
hbeddouka <50579195+hbeddouka@users.noreply.github.com>
2019-11-29 10:26:51 +0100

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -5,8 +5,7 @@ define("CLASS_PATH", dirname($_SERVER['PHP_SELF'])."/");
 define("APPLICATION_PATH", dirname($_SERVER['PHP_SELF'])."/");
 
 //Configuración de la conexion a la base de datos
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST').":".getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
